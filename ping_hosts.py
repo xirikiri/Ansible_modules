58ec5a40fd45155d937fc1cabd39d5e51b1c886a
xirikiri <ilia.bogatyrev@gmail.com>
2020-06-11 00:21:16 +0300

    some changes v4

--- ping_hosts.py
+++ ping_hosts.py
@@ -2,51 +2,54 @@ from ansible.module_utils.basic import AnsibleModule
 import subprocess
 import re
 
-#Формат запуска модуля из консоли
-#ansible all -m ping_hosts -a "hosts=ya.ru,mail.ru"
-#Предварительно установить ansible на локальной машине
-#Закинуть модуль в папку с модулями
+
+# Формат запуска модуля из консоли
+# ansible all -m ping_hosts -a "hosts=ya.ru,mail.ru"
+# Предварительно установить ansible на локальной машине
+# Закинуть модуль в папку с модулями
 
 def run_module():
-    #Аргументы модуля
+    # Аргументы модуля
     module_args = dict(
         hosts=dict(type='list', required=True)
     )
-    #Формат ответа модуля
+    # Формат ответа модуля
     result = dict(
         changed=False,
         response=''
     )
-    #Инстанцируем модуль
+    # Инстанцируем модуль
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
     )
-    #Создаем регулярное выражение
+    # Создаем регулярное выражение
     regex = re.compile('(\\n)')
-    answer =''
-    #Пробегаемся по нужным хостам
+    answer = ''
+    # Пробегаемся по нужным хостам
     for x in module.params['hosts']:
-        #Формируем подпроцесс на удаленном узле
-        result_ping = subprocess.Popen(["ping", x, "-c", "1"],stdout=subprocess.PIPE)
-        #Ждем его окончания
+        # Формируем подпроцесс на удаленном узле
+        result_ping = subprocess.Popen(["ping", x, "-c", "1"], stdout=subprocess.PIPE)
+        # Ждем его окончания
         result_ping.wait()
-        #Читаем результат выполнения процесса
+        # Читаем результат выполнения процесса
         temp_result = result_ping.stdout.read()
-        #Разбиваем регулярным выражением ответ комманды
+        # Разбиваем регулярным выражением ответ комманды
         arrayresponse = regex.split(temp_result)
-        #Достаем последнюю строчку с помощью регулярки и прибавляем ее к предыдущим
-        if list.count(arrayresponse)>2:
-            answer = answer + x +':' + regex.split(temp_result)[-3] + ';'
+        # Достаем последнюю строчку с помощью регулярки и прибавляем ее к предыдущим
+        if len(arrayresponse) > 2:
+            answer = answer + x + ':' + regex.split(temp_result)[-3] + ';'
         else:
-            answer = answer + x +':' + 'NOT AVAILABLE' + ';'
-    #Устанавливаем ответ молуля
+            answer = answer + x + ':' + 'NOT AVAILABLE' + ';'
+    # Устанавливаем ответ молуля
     result['response'] = answer
-    #Завершаем работу модуля
+    # Завершаем работу модуля
     module.exit_json(**result)
+
 
 def main():
     run_module()
+
 
 if __name__ == '__main__':
     main()
